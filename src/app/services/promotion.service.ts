0e9a2a1e5f81c8d847ffcdec04047a8b08f33b36
EvokSix <ferreira.julhinho@gmail.com>
2022-05-12 10:49:36 -0300

    adicionado dialog de login

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Dish } from '../shared/dish';
 
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+
 
 @Injectable({
   providedIn: 'root'
