ce920421a374ce6307c099fbc73a3fcf1d3dfae6
EvokSix <ferreira.julhinho@gmail.com>
2022-05-13 18:06:37 -0300

    adicionado paginação usando params observable

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
 
 import { Dish } from '../shared/dish';
 
@@ -16,6 +17,9 @@ import { DishService } from '../services/dish.service';
 export class DishdetailComponent implements OnInit {
 
   dish!: Dish;
+  dishIds!: string[];
+  prev!: string;
+  next!: string;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -24,9 +28,17 @@ export class DishdetailComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.params['id'];
-    this.dishservice.getDish(id.toString())
-      .subscribe(dish => this.dish = dish);
+    this.dishservice.getDishIds()
+      .subscribe((dishIds) => this.dishIds = dishIds);
+    this.route.params
+      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(dishId: string){
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack(): void {
