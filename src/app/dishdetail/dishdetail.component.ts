ea94665564c60b8bed411a6775898cb3ea8f03ea
EvokSix <ferreira.julhinho@gmail.com>
2022-05-17 15:44:56 -0300

    adicionado animações parte 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, NgForm} from '@angular/forms';
+import { trigger, state,style, animate, transition } from '@angular/animations';
 import { switchMap } from 'rxjs/operators';
 
 import { Dish } from '../shared/dish';
@@ -15,7 +16,21 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.css']
+  styleUrls: ['./dishdetail.component.css'],
+  animations:[
+    trigger('visibility',[
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -28,8 +43,24 @@ export class DishdetailComponent implements OnInit {
   form!: FormGroup;
   comment!: Comment;
   dishcopy!: Dish;
+  visibility = 'shown';
 
-  @ViewChild('fform') commentFormDirective!:NgForm;
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Author Name is  required.',
+      'minlenght': 'Author Name must be at least 2 characteres.'
+    },
+    'comment':{
+      'required': 'Comment is required.'
+    }
+  };
+
+  @ViewChild('cform') commentFormDirective!:NgForm;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -42,8 +73,8 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds()
       .subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+      .pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(params['id']); }))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
       errmess => this.errMess = <any> errmess);
     this.createForm();
   }
